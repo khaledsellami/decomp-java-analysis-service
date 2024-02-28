080e0f1efe059371c037c603ba653f0b9f91be7d
khalsel <khaled.sellami.1@ulaval.ca>
2024-02-28 15:58:19 -0500

    clone source code when using api

--- src/main/java/AnalysisCLI.java
+++ src/main/java/AnalysisCLI.java
@@ -4,6 +4,8 @@ import picocli.CommandLine;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @CommandLine.Command(name = "analyze", mixinStandardHelpOptions = true,
         description = "Statically analyzes a monolithic application through the client line interface.")
@@ -13,7 +15,7 @@ public class AnalysisCLI implements Runnable {
     private String appName;
     @CommandLine.Option(
             names = {"-p", "--path"},
-            description = "The path to source code of the application",
+            description = "The path to source code of the application or link to the repository",
             required = false)
     private String appPath;
 
@@ -41,6 +43,7 @@ public class AnalysisCLI implements Runnable {
         }
         if (!dataLoader.exists(appName)){
             if (appPath == null){
+                logger.info("Loading source code from default path!");
                 RepoHandler repoHandler = new RepoHandler(appName, "");
                 try{
                     appPath = repoHandler.getOrClone();
@@ -50,6 +53,21 @@ public class AnalysisCLI implements Runnable {
                     return;
                 }
             }
+            else {
+                if (isURL(appPath)){
+                    logger.info("Cloning source code from link!");
+                    RepoHandler repoHandler = new RepoHandler(appName, appPath);
+                    try{
+                        appPath = repoHandler.getOrClone();
+                    }
+                    catch(IOException e){
+                        logger.info("Encountered error when loading the source code: \""+e.getMessage()+"\"!");
+                        return;
+                    }
+                }
+                else
+                    logger.info("Loading source code from the given path!");
+            }
             try{
                 dataLoader.analyze(appName, appPath);
             }
@@ -58,5 +76,17 @@ public class AnalysisCLI implements Runnable {
             }
         }
         dataLoader.restoreDefaultOutputPath();
+    }
+
+    private Boolean isURL(String pathOrURL) {
+        String regex = "^(?:http|ftp)s?://" +                  // http:// or https://
+                "(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\\.)+(?:[A-Z]{2,6}\\.?|[A-Z0-9-]{2,}\\.?)|" +  // domain...
+                "localhost|" +                           // localhost...
+                "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})" +  // ...or ip
+                "(?::\\d+)?" +                          // optional port
+                "(?:/?|[/?]\\S+)$";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(pathOrURL);
+        return matcher.find();
     }
 }
