01c245bd2bb541f76c85589a7e0374ba03f190fc
khalsel <khaled.sellami.1@ulaval.ca>
2024-02-29 13:28:50 -0500

    version 1.2.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import picocli.CommandLine;
 
 @CommandLine.Command(name = "st_analyzer", synopsisSubcommandLabel = "COMMAND",
         subcommands = {AnalysisCLI.class, AnalysisServer.class},
-        mixinStandardHelpOptions = true, version = "1.1.1",
+        mixinStandardHelpOptions = true, version = "1.2.0",
         description = "Statically analyzes a monolithic application.")
 public class Main implements Runnable {
 
